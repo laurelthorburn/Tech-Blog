3131bcf1961be4c7dcfe243e31b531021b632619
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-24 17:45:18 -0700

    Added bcrypt code for password in user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -30,8 +34,14 @@ User.init(
         len: [6],
       },
     },
-  },
+  },   
   {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
