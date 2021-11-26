154ada7edf8c7497c074a471ae73c6cf1e61fdde
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-26 12:33:25 -0700

    creating POST and log in controllers

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,28 +2,81 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../../models/User');
 
-// // CREATE a new user
-// router.post('/', async (req, res) => {
-//   try {
-//     const newUser = req.body;
-//     // hash the password from 'req.body' and save to newUser
-//     newUser.password = await bcrypt.hash(req.body.password, 10);
-//     // create the newUser with the hashed password and save to DB
-//     const userData = await User.create(newUser);
-//     res.status(200).json(userData);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+// router.get('/', (req, res) => {
+//   //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+//   res.render('all');
+//   });
 
-// router.get("/", (req, res) => {
-//   res.render("all");
-// })
+// CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
 
+    req.session.save(() => {
+      req.session.loggedIn = true;
 
-router.get('/', (req, res) => {
-  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-  res.render('all');
-  });
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'USERNAMETEST Incorrect username or password. Please try again!' });
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'PASSWORDTEST Incorrect username or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      console.log(
+        '🚀 ~ file: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie',
+        req.session.cookie
+      );
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
