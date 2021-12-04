68689fa59eb7fca39df263a0caa24bf69909390c
Laurel Thorburn <laurelthorburn@gmail.com>
2021-12-03 17:11:19 -0700

    Fixed commented out code

--- public/js/post.js
+++ public/js/post.js
@@ -16,7 +16,7 @@ const newFormHandler = async (event) => {
      });
  
      if (response.ok) {
-      //  document.location.replace('/dashboard');
+       document.location.replace('/dashboard');
      } else {
        alert('Failed to create post');
      }
